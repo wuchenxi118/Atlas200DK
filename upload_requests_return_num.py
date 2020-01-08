202748dff9efb4a5c95ff3ff9d3d95c9ec62a8ed
wuchenxi118 <wuchenxi118@126.com>
2020-01-08 20:38:44 +0800

    upload a new request file

--- upload_requests_return_num.py
+++ upload_requests_return_num.py
@@ -13,11 +13,12 @@ def convertJPG_to_string(img_path,ask_url,scale_size=[300,300]):
     :return: None
     """
     cv_img = cv2.imread(img_path)
-    cv_img = cv2.resize(cv_img,(scale_size[0],scale_size[1]))
+
+    # cv_img = cv2.resize(cv_img,(scale_size[0],scale_size[1]))
     img_encoded = cv2.imencode('.jpg',cv_img)[1].tostring()
     res = requests.post(ask_url, data=img_encoded)
     print(res.text)
 
 if __name__ == '__main__':
-    convertJPG_to_string('/home/wuchenxi/PycharmProjects/Atlas200DK/sheep.jpg',
+    convertJPG_to_string('/home/wuchenxi/PycharmProjects/Atlas200DK/28.jpg',
                          ask_url='http://192.168.8.151:120')
